ccfc828856c1a5c5b98b9af54c1d55a9f8ff7e89
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2023-01-02 00:34:23 +0530

    rewrote the code

--- src/main/scala/n_adder.scala
+++ src/main/scala/n_adder.scala
@@ -9,37 +9,14 @@ class n_adder(N: Int = 4) extends Module {
 	val S = IO(Output(SInt(N.W)))
 	val co = IO(Output(UInt(1.W)))
 
-	val C = Array.fill(N+1){ Wire(UInt(1.W)) }
-	C(0) := ci
+	// instantiating adders and connecting bus A and B bits
+	val adders = Seq.tabulate(N)(i => (Module(new fulladder), A(i), B(i)))
+		.map{ case(adder, a_i, b_i) => {adder.a := a_i; adder.b := b_i; adder} }
 
-	/*
-		Chisel doesn't support individually assigning values
-		to wires in a bus(pro or con? don't know)
-	*/
-	/*
-		tabulate is similar to for loop. It generates element using integers
-		0 - (N-1) then bundles them into an array.
-		In this case its creates an array of "sum" outputs of the fulladders
-		Which is then concatenated to the bus output "S"
-	*/
-	S := Cat(Seq.tabulate(N)(i => {
-		val adder = Module(new fulladder)
-		adder.a := A(i).asUInt
-		adder.b := B(i).asUInt
-		adder.ci := C(i)
-		C(i+1) := adder.co
-		adder.sum // creating an array of bit, then contatenating using Cat()
-	}).reverse).asSInt
-	/*
-		".reverse" is needed because Cat() function actually works in the wrong order
-		it concatenates the bus in the oppesite order.
-		Not sure if I'm the one thats thinking in reverse, or if this is a problem that
-		really should be solved before releasing. There is a good chance a lot code bases
-		use Cat() this way. And solving this might cause alot of code bases to change.
-		This looks like a problem that needs to be asked from the creators of Chisel.
-	*/
-
-	co := C(N)
+	// connecting the carry bits and extracting the final carry bit
+	co := adders.foldLeft(ci)((carr_in, adder) => { adder.ci := carr_in; adder.co })
+	// getting the sum bit(without ".reverse" it bundles the bits in the wrong direction)
+	S := Cat(adders.map(_.sum).reverse).asSInt
 }
 
 object n_adder extends App {
